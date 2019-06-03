acfab84e1764e2889582a9a805e20a22e87a7cbf
Mykal Clinard <mykalclinard@gmail.com>
2019-06-03 13:51:39 -0500

    exercise finish

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,8 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(255) NOT NULL,
     name  VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL,
-    sales float NOT NULL,
+    release_date int NOT NULL,
+    sales decimal(10, 2) NOT NULL,
     genre varchar(50) NOT NULL,
     PRIMARY KEY (id)
 );
