e26d165bd451ba82e5631cbcb5eede3e6c71f173
Mykal Clinard <mykalclinard@gmail.com>
2019-06-04 11:38:32 -0500

    finished exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,28 +1,28 @@
 USE codeup_test_db;
 
 select artist 'Name of Artist', name 'Name of Album'
-from albums;
+    from albums;
 
 select artist 'Name of Artist', name 'Name of Album', release_date
-from albums
-where release_date > 1991;
-delete
-from albums
-where release_date > 1991;
+    from albums
+    where release_date > 1991;
+#deletes
+delete from albums
+    where release_date > 1991;
 
 select name, genre
-from albums
-where genre like '%disco%';
-delete
-from albums
-where genre like '%disco%';
+    from albums
+    where genre like '%disco%';
+#deletes
+delete from albums
+    where genre like '%disco%';
 
 select name 'Whitney Houston Songs', release_date
-from albums
-where artist = 'Whitney Houston';
-delete
-from albums
-where artist = 'Whitney Houston';
+    from albums
+    where artist = 'Whitney Houston';
+#deletes
+delete from albums
+    where artist = 'Whitney Houston';
 
 
 
