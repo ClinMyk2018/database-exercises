39405e1b38bbac5786ea72dffd4e6120801552c7
Mykal Clinard <mykalclinard@gmail.com>
2019-06-04 11:26:53 -0500

    finished exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -6,24 +6,24 @@ from albums;
 select artist 'Name of Artist', name 'Name of Album', release_date
 from albums
 where release_date > 1991;
+delete
+from albums
+where release_date > 1991;
 
 select name, genre
+from albums
+where genre like '%disco%';
+delete
 from albums
 where genre like '%disco%';
 
 select name 'Whitney Houston Songs', release_date
 from albums
 where artist = 'Whitney Houston';
-
-
-delete
-from albums
-where release_date > 1991;
-
-delete
-from albums
-where genre like '%disco%';
-
 delete
 from albums
 where artist = 'Whitney Houston';
+
+
+
+
